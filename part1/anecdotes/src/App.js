74fa73ccafdccce93880e7324aaf69b3d93f4128
sayyed-akhtar <sayyed.akhtar@hazesoft.co>
2023-01-15 22:18:16 +0545

    1.13*: anecdotes step2, expanded application so that we can vote for the displayed anecdote.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,20 @@
 import { useState } from 'react';
 
+const Vote = ({ voteCount }) => {
+  if (voteCount > 0) {
+    return (
+      <h4>
+        has {voteCount} {voteCount > 1 ? 'votes' : 'vote'}
+      </h4>
+    );
+  }
+  return <h4>has no vote!</h4>;
+};
+
+const Button = ({ text, onClickHandler }) => (
+  <button onClick={onClickHandler}>{text}</button>
+);
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -10,18 +25,27 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
   ];
+  const points = new Array(anecdotes.length).fill(0);
 
   const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(points);
 
-  const handleClick = () => {
+  const handleAnecdotes = () => {
     const random = Math.floor(Math.random() * anecdotes.length);
     setSelected(random);
+  };
+
+  const handleVote = () => {
+    vote[selected] += 1;
+    setVote([...vote]);
   };
 
   return (
     <div>
       <h2>{anecdotes[selected]}</h2>
-      <button onClick={handleClick}>next anecdotes</button>
+      <Vote voteCount={vote[selected]} />
+      <Button text='vote' onClickHandler={handleVote} />
+      <Button text='next anecdotes' onClickHandler={handleAnecdotes} />
     </div>
   );
 };
